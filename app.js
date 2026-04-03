813b188a3cce72ab48334a874d1af5c6c40c21e5
OliverDitlev <oldi24ab@student.cbs.dk>
2026-04-03 15:55:56 +0200

    ty

--- app.js
+++ app.js
@@ -4,6 +4,3 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
